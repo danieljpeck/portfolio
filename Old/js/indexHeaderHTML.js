3f32a0fec1903bdcad12e0f42050d4228b3ef497
danieljpeck <daniel.jerome.peck@gmail.com>
2018-09-30 14:36:10 -0700

    Added handlebars as package for deployment

--- Old/js/indexHeaderHTML.js
+++
@@ -1,9 +0,0 @@
-
-window.onload = function() {
-document.getElementById("typewriterContent").innerHTML = '<h1>My name is Daniel.<br></h1>
-                <h1>I am a <i>
-                    <a href="" class="typewrite" data-period="2000" data-type='[ "designer.", "storyteller.", "developer.", "innovator.", "unicorn?" ]'>
-                    <span class="wrap"></span>
-                    </a>
-                </i></h1>';
-    };
