def9f16d28d8fe4f2169753a482e12424a30df7c
Daniel <daniel.jerome.peck@gmail.com>
2021-03-02 23:53:56 -0800

    030221 updates

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,34 @@
+const {series} = require('gulp');
 const gulp = require('gulp');
 const browserSync = require('browser-sync');
 const sass = require('gulp-sass');
 const nodemon = require('nodemon');
  
 sass.compiler = require('node-sass');
- 
-gulp.task('sass:watch', function () {
-  gulp.watch('./assets/**/*.scss', ['sass']);
-});
 
-
-gulp.task('default', ['browser-sync'], function () {
-});
-
-gulp.task('browser-sync', ['nodemon'], function() {
-	gulp.watch("assets/scss/**.*", ['sass']);
-  gulp.watch("views/**/*.hbs").on('change', browserSync.reload);
-});
-gulp.task('nodemon', ['sass'], function (cb) {
-  return nodemon({
-    script: 'server.js'
-  }).once('start', cb); // once only get's run........... <drum role>........ once :D
-});
-gulp.task('sass', function() {
+function sassCompile() {
   return gulp.src("assets/scss/*.scss")
       .pipe(sass())
       .pipe(gulp.dest("assets/css"))
       .pipe(browserSync.stream());
-});
+}
+
+function sassWatch() {
+  gulp.watch('./assets/**/*.scss', gulp.series('sassCompile'));
+}
+
+function browserSyncFunc() {
+  gulp.watch("assets/scss/**.*", gulp.series('sassCompile'));
+  gulp.watch("views/**/*.hbs").on('change', browserSync.reload);
+}
+
+function nodemonFunc(cb) {
+  return nodemon({
+    script: 'server.js'
+  }).once('start', cb); // once only get's run........... <drum role>........ once :D
+}
+
+exports.default = series(sassCompile, nodemonFunc, browserSyncFunc);
+exports.sassWatch= sassWatch;
+exports.sassCompile = sassCompile;
+
